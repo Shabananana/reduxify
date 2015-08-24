a8d9f14c6c9a63976d0d10b40d12751f8b0ceb89
Jeff Davis <jdavis@draftkings.com>
2015-08-24 17:44:36 -0400

    Initial setup for isomorphic implimentation

---
+++ hapi-server.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Root from './common/containers/Root';
+import Router from 'react-router';
+import MemoryHistory from 'react-router/lib/MemoryHistory';
+import Location from 'react-router/lib/Location';
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 9000 });
+
+server.route({
+    method: 'GET',
+    path: '/dist/{filename}',
+    handler: function (request, reply) {
+        reply.file('./dist/' + request.params.filename);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: function (request, reply) {
+      const html = React.renderToString(
+        <Root history={new MemoryHistory([ 'test', '/' ])} />
+      );
+      reply(renderFullPage(html, null));
+    }
+});
+
+function renderFullPage(html, initialData) {
+  return `<!doctype html>
+          <html>
+            <head>
+              <title>Test UGC App!</title>
+              <style>
+                .selected {
+                  background-color: pink;
+                }
+                .filled {
+                  background-color: red;
+                }
+              </style>
+            </head>
+            <body>
+              <h1>Test UGC App!</h1>
+              <div id="root">${html}</div>
+              <script>
+                __INITIAL_DATA__ = ${JSON.stringify(initialData)};
+              </script>
+              <script src="./dist/bundle.js"></script>
+            </body>
+          </html>`
+}
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
